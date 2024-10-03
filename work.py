7b5b2218c933844b26946fdc34b52ba63ebd5d61
trinityvthompson <trinityvthompson@utexas.edu>
2024-10-03 16:36:53 -0500

    Update work.py

--- work.py
+++ work.py
@@ -1,3 +1,7 @@
+# Assignment 5
+# Trinity Thompson tyt242
+# Marissa Shuchart ms87339
+
 import sys
 import time
 
@@ -7,23 +11,43 @@ import time
 # Output: computes the sum of the series (v + v // k + v // k**2 + ...)
 #         returns the sum of the series
 def sum_series (v, k):
+  total = 0 # Initialize total number of lines written
+  factor = 1 # Factor that controls productivity decay (starts at 1)
 
+  while v // factor > 0: # Continue as long as Chris can write lines of code 
+    total += v // factor # Add number of lines Chris can write given the current productivity
+    factor *= k # Decrease productivity
 
+  return total
 
 # Input: n an integer representing the total number of lines of code
 #        k an integer representing the productivity factor
 # Output: returns v the minimum lines of code to write using linear search
 def linear_search (n, k):
+  v = 1
 
-
+  while sum_series(v, k) < n: # While lines written is less than lines needed to write, increment v by 1
+    v += 1
+  
+  return v
 
 # Input: n an integer representing the total number of lines of code
 #        k an integer representing the productivity factor
 # Output: returns v the minimum lines of code to write using binary search
 def binary_search (n, k):
+  # Initialize a low and high value
+  low = 1
+  high = n
 
+  while low < high:
+    midpoint = (low + high) // 2
 
+    if sum_series(midpoint, k) < n:
+      low = midpoint + 1
+    else:
+      high = midpoint
 
+  return low
 
 def main():
   # read number of cases
